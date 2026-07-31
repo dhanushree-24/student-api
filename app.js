dbf9878567978418cb35371ba1171a5a144c41b4
dhanushree-24 <dhanushree24112006@gmail.com>
2026-07-31 15:43:22 +0530

    port Setting change 1

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get("/", (req, res) => {
 
 const  PORT= process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Server is running on ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
 });
