3e7d395c0105dcbcf6ac7db21bf1b31a29457669
dhanushree-24 <dhanushree24112006@gmail.com>
2026-07-31 15:39:46 +0530

    new change

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
     res.send("Welcome to github actions");
 });
 
-const port = process.env.PORT || 3000;
+const  PORT= process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
